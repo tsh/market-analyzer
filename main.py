11cd9bdf3f8f23d82605c4b496a50fa56cfec970
tsh <dr.tallin@gmail.com>
2021-12-03 23:20:02 +0200

    ibkr

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from views import GUIView, TerminalView
 
 from pprint import pprint
 
+
 if __name__ == '__main__':
-    qqq = Stock('GOOG')
+    # qqq = Stock('GOOG')
     # gv = GUIView()
     # gv.display(qqq)
-    tv = TerminalView()
-    tv.display([qqq.name, qqq.ask()])
-
+    # tv = TerminalView()
+    # tv.display([qqq.name, qqq.ask()])
 
--- models.py
+++ models.py
@@ -6,6 +6,17 @@ import yfinance as yf
 from config import DATA_DIR
 
 
+class IBKRProvider():
+    def __init__(self):
+        import requests
+        resp = requests.get('https://localhost:5000/v1/api/portfolio/accounts/', verify=False)
+        acc = resp.json()
+        acc_id = acc[0]['accountId']
+        positions = requests.get('https://localhost:5000/v1/api/portfolio/{accountId}/positions/{pageId}'.format(accountId=acc_id, pageId=0), verify=False)
+        print(positions.json())
+        "[{'acctId': 'U4348872', 'conid': 11520, 'contractDesc': 'RCL', 'position': 32.0, 'mktPrice': 84.5763321, 'mktValue': 2706.44, 'currency': 'USD', 'avgCost': 73.6125, 'avgPrice': 73.6125, 'realizedPnl': 0.0, 'unrealizedPnl': 350.84, 'exchs': None, 'expiry': None, 'putOrCall': None, 'multiplier': None, 'strike': 0.0, 'exerciseStyle': None, 'conExchMap': [], 'assetClass': 'STK', 'undConid': 0, 'model': ''},"
+
+
 class Stock:
     def __init__(self, name: str):
         self.name = name
