223b7b60ae588ab172d147a6d24b7445a237dd33
tsh <dr.tallin@gmail.com>
2022-08-09 21:35:21 +0300

    edgar urls

---
+++ edgar.py
@@ -0,0 +1,24 @@
+class CIK:
+    """https://sec.report/Ticker/AAPL"""
+    pass
+
+
+class CIK2Report:
+    """
+    https://www.sec.gov/edgar/sec-api-documentation
+    https://data.sec.gov/submissions/CIK0000320193.json
+    """
+    pass
+
+class Agent:
+    """User-Agent: Sample Company Name AdminContact@<sample company domain>.com
+    Accept-Encoding: gzip, deflate"""
+
+
+"""
+Latest:
+https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&datea=&dateb=&company=&type=4&SIC=&State=&Country=&CIK=&owner=include&accno=&start=0&count=100
+DailyIndex:
+https://www.sec.gov/Archives/edgar/daily-index/2022/QTR3/
+"""
+
