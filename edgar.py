2314be63a01906cae457df15a93edfa087636254
tsh <dr.tallin@gmail.com>
2022-08-12 00:39:33 +0300

    edgar daily archive

--- edgar.py
+++ edgar.py
@@ -26,17 +26,33 @@ class Recent:
     Latest:
     https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&datea=&dateb=&company=&type=4&SIC=&State=&Country=&CIK=&owner=include&accno=&start=0&count=100
     or via RSS: https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&CIK=&type=&company=&dateb=&owner=include&start=0&count=40&output=atom
+    or archive https://www.sec.gov/Archives/edgar/Feed/2022/QTR3/
     """
     report = 4
     url = 'https://www.sec.gov/cgi-bin/browse-edgar'
     params ='action=getcurrent&datea=&dateb=&company=&type=4&SIC=&State=&Country=&CIK=&owner=include&accno=&start=0&count=100'
     r = requests.get(url, params=params, headers=headers)
 
-class Daily:
-    def __init__(self):
-        url = 'https://www.sec.gov/Archives/edgar/daily-index/2022/QTR3/'
+class DailyArchive:
+    @staticmethod
+    def date_to_url(year, month, date):
+        quarter = pd.Timestamp(year, month, date).quarter
+        return f'https://www.sec.gov/Archives/edgar/daily-index/{year}/QTR{quarter}'
 
+    @classmethod
+    def get_url_forms_per_day(cls, year, month, date):
+        url = cls.date_to_url(year, month, date)
+        t = pd.Timestamp(year, month, date)
+        filename = 'form.'+t.strftime('%Y%m%d')
+        return url + '/' + filename + '.idx'
+
+    def get_report_per_day(self, year, month, date):
+        url = Daily.get_url_forms_per_day(year, month, date)
+        resp = requests.get(url, headers=headers)
+        resp.raise_for_status()
+        data = resp.content
+        return data
 
 if __name__ == '__main__':
-    t = pd.Timestamp(2022, 8, 11)
-    print(t, t.quarter)
+    d = Daily().get_report_per_day(2022, 8, 9)
+    print(d)
