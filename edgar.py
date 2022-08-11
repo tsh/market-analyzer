45b4e9ae3f0824af9f61575f06379a97ebaa9074
tsh <dr.tallin@gmail.com>
2022-08-11 21:43:32 +0300

    edgar urls

--- edgar.py
+++ edgar.py
@@ -1,3 +1,7 @@
+import requests
+import pandas as pd
+
+
 class CIK:
     """https://sec.report/Ticker/AAPL"""
     pass
@@ -15,10 +19,24 @@ class Agent:
     Accept-Encoding: gzip, deflate"""
 
 
-"""
-Latest:
-https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&datea=&dateb=&company=&type=4&SIC=&State=&Country=&CIK=&owner=include&accno=&start=0&count=100
-DailyIndex:
-https://www.sec.gov/Archives/edgar/daily-index/2022/QTR3/
-"""
+headers = {'User-Agent': 'Market-Analyzer', 'Accept': 'application/json'}
 
+class Recent:
+    """
+    Latest:
+    https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&datea=&dateb=&company=&type=4&SIC=&State=&Country=&CIK=&owner=include&accno=&start=0&count=100
+    or via RSS: https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&CIK=&type=&company=&dateb=&owner=include&start=0&count=40&output=atom
+    """
+    report = 4
+    url = 'https://www.sec.gov/cgi-bin/browse-edgar'
+    params ='action=getcurrent&datea=&dateb=&company=&type=4&SIC=&State=&Country=&CIK=&owner=include&accno=&start=0&count=100'
+    r = requests.get(url, params=params, headers=headers)
+
+class Daily:
+    def __init__(self):
+        url = 'https://www.sec.gov/Archives/edgar/daily-index/2022/QTR3/'
+
+
+if __name__ == '__main__':
+    t = pd.Timestamp(2022, 8, 11)
+    print(t, t.quarter)
