23aedcd129b17fe85525d541571c97ce18e44a12
tsh <dr.tallin@gmail.com>
2022-08-13 22:19:31 +0300

    parse rss

--- edgar.py
+++ edgar.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+import xml.etree.ElementTree as ET
 
 
 class CIK:
@@ -25,13 +26,29 @@ class Recent:
     """
     Latest:
     https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&datea=&dateb=&company=&type=4&SIC=&State=&Country=&CIK=&owner=include&accno=&start=0&count=100
-    or via RSS: https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&CIK=&type=&company=&dateb=&owner=include&start=0&count=40&output=atom
     or archive https://www.sec.gov/Archives/edgar/Feed/2022/QTR3/
+    RSS: updated every ten minutes Monday through Friday, 6am – 10pm EST
     """
-    report = 4
-    url = 'https://www.sec.gov/cgi-bin/browse-edgar'
-    params ='action=getcurrent&datea=&dateb=&company=&type=4&SIC=&State=&Country=&CIK=&owner=include&accno=&start=0&count=100'
-    r = requests.get(url, params=params, headers=headers)
+    @classmethod
+    def get_recent(cls):
+        url = 'https://www.sec.gov/cgi-bin/browse-edgar'
+        params = {
+            'action': 'getcurrent',
+            'type': 4,
+            'owner': 'include',
+            'start': 0,
+            'count': 10,
+            'output': 'atom'
+        }
+        r = requests.get(url, params=params, headers=headers)
+        r.raise_for_status()
+        import feedparser
+        feed = feedparser.parse(r.content)
+        print(feed)
+
+    def extract_data(self, document):
+        pass
+
 
 class DailyArchive:
     @staticmethod
@@ -54,5 +71,38 @@ class DailyArchive:
         return data
 
 if __name__ == '__main__':
-    d = Daily().get_report_per_day(2022, 8, 9)
-    print(d)
+    url = 'https://www.sec.gov/cgi-bin/browse-edgar'
+    params = {
+        'action': 'getcurrent',
+        'type': 4,
+        'owner': 'include',
+        'start': 0,
+        'count': 10,
+        'output': 'atom'
+    }
+    r = requests.get(url, params=params, headers=headers)
+    r.raise_for_status()
+    # parse
+    import feedparser
+    feed = feedparser.parse(r.content)
+    submission = {}
+    for s in feed.entries:
+        submission['link'] = s.link
+        submission['title'] = s.title
+        break
+    # submission page
+    r = requests.get(submission['link'], headers=headers)
+    r.raise_for_status()
+    from bs4 import BeautifulSoup
+    soup = BeautifulSoup(r.content, 'html')
+    report_links = []
+    for a in soup.find_all('a'):
+        link = a.get('href')
+        if link.startswith('/Archives') and link.endswith('.xml') and a.text.endswith('.xml'):
+            report_links.append(link)
+    # get report
+    for report_link in report_links:
+        r = requests.get('https://www.sec.gov' + report_link, headers=headers)
+        r.raise_for_status()
+
+
