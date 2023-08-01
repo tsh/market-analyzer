dcd5afb3393f18847c8067c7f98195858d905156
tsh <dr.tallin@gmail.com>
2023-08-01 22:55:23 +0300

    add options

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from edgar.edgar import RecentSubmissionsAtom
 
 from pprint import pprint
 
-
-if __name__ == '__main__':
+def tg():
     interests = ['tesla', 'scion']
     activities = []
     for parser in RecentSubmissionsAtom().get_data():
@@ -20,4 +19,23 @@ if __name__ == '__main__':
     else:
         text = 'Nothing found'
     tg.send(text)
+
+
+
+if __name__ == '__main__':
+    import yfinance as yf
+    res = {}
+    for stock in ['BITO', 'APLD']:
+        ticker = yf.Ticker(stock)
+        opts = ticker.options
+        last_date = opts[-1]
+        chain = ticker.option_chain(last_date)
+        res[stock] = [chain.calls, ticker.history()]
+
+    for s, v in res.items():
+        df = v[0]
+        dff = df.loc[df['inTheMoney'] == False]
+        last_history = v[1].tail(1)
+        print(s, last_history.index.to_list()[0].strftime('%Y-%m-%d'),
+              round(last_history.iloc[0].High, 2), round(dff.impliedVolatility.mean(), 2))
 
