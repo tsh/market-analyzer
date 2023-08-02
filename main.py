b479f238465afb7f95f6e3a5b103dc431636c3e4
tsh <dr.tallin@gmail.com>
2023-08-02 14:20:45 +0300

    print iv

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from models import Stock, YahooProvider
 from views import GUI, TUI, Telegram
 from edgar.edgar import RecentSubmissionsAtom
 
+from rich.table import Table
+from rich.console import Console
 from pprint import pprint
+
 
 def tg():
     interests = ['tesla', 'scion']
@@ -20,22 +23,37 @@ def tg():
         text = 'Nothing found'
     tg.send(text)
 
-
-
-if __name__ == '__main__':
+def get_data():
     import yfinance as yf
-    res = {}
+    data = {}
     for stock in ['BITO', 'APLD']:
         ticker = yf.Ticker(stock)
         opts = ticker.options
         last_date = opts[-1]
         chain = ticker.option_chain(last_date)
-        res[stock] = [chain.calls, ticker.history()]
+        data[stock] = [chain.calls, ticker.history()]
 
-    for s, v in res.items():
+    res ={}
+    for s, v in data.items():
         df = v[0]
         dff = df.loc[df['inTheMoney'] == False]
         last_history = v[1].tail(1)
-        print(s, last_history.index.to_list()[0].strftime('%Y-%m-%d'),
-              round(last_history.iloc[0].High, 2), round(dff.impliedVolatility.mean(), 2))
+        res[s]= {'date': last_history.index.to_list()[0].strftime('%Y-%m-%d'),
+                 'price': round(last_history.iloc[0].High, 2),
+                 'iv': round(dff.impliedVolatility.mean(), 2)}
+    return res
+
+
+if __name__ == '__main__':
+        table = Table(show_header=True, header_style="bold ")
+        table.add_column("Ticker", justify="right", style="cyan", no_wrap=True)
+        table.add_column("Date", justify="right",  no_wrap=True)
+        table.add_column("Price H", justify="right", no_wrap=True)
+        table.add_column("IV", justify="right", no_wrap=True)
+
+        stock_data = get_data()
+        for stock, data in stock_data.items():
+            table.add_row(stock, data['date'], str(data['price']), str(data['iv']))
+        console = Console()
+        console.print(table)
 
