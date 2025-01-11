d671bbda5c9c0c4b806a1e58563d10a6ab1b30fb
tsh <tsh@tsh.tsh>
2025-01-11 23:03:09 +0700

    get publication date

--- crawlers/vic/main.py
+++ crawlers/vic/main.py
@@ -1,7 +1,9 @@
 import sys
+from datetime import datetime
 from pathlib import Path
 
 from bs4 import BeautifulSoup, SoupStrainer
+import dateutil
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -15,7 +17,6 @@ sys.path.append(str(project_dir))
 import config as cfg
 
 URL_VIC = 'http://valueinvestorsclub.com'
-URL_VIC_IDEAS = f'{URL_VIC}/ideas'
 
 
 
@@ -29,9 +30,10 @@ class Driver:
         # service.executable_path = '/lib/chromedriver'
         self.driver = webdriver.Chrome(options=options)
 
-    def get(self, url, wait_selector: tuple, wait_timeout=6) -> str:
+    def get(self, url, wait_selector: tuple=None, wait_timeout=6) -> str:
         self.driver.get(url)
-        WebDriverWait(self.driver, wait_timeout).until(lambda drver: drver.find_element(*wait_selector).is_displayed())
+        if wait_selector:
+            WebDriverWait(self.driver, wait_timeout).until(lambda drver: drver.find_element(*wait_selector).is_displayed())
         return self.driver.page_source
 
     def __enter__(self):
@@ -41,20 +43,41 @@ class Driver:
         self.driver.quit()
 
 
-class VICIdeasParser:
+class Parser:
     def __init__(self, page_content: str):
         self.page_content = page_content
+        self.soup = BeautifulSoup(self.page_content, 'html.parser')
 
+
+class VICIdeasParser(Parser):
     def get_ideas_links(self) -> list:
         links = []
-        for link in  BeautifulSoup(self.page_content, 'html.parser').find(id='ideas_body').find_all('a'):
+        for link in  self.soup.find(id='ideas_body').find_all('a'):
             links.append(link.get('href'))
         return links
 
 
-selector = (By.CSS_SELECTOR, "#ideas_body>.row")
+class IdeaParser(Parser):
+    def get_author_url(self) -> str:
+        return self.soup.find('div', attrs={'class': 'idea_by'}).find('a').get('href')
+
+    def get_publication_date(self):
+        raw_text = self.soup.find('div', attrs={'class': 'idea_by'}).find('div').text
+        text = raw_text[:-3]  # rm `by`
+        return dateutil.parser.parse(text)
+
+
 with Driver() as d:
-    content = d.get(URL_VIC_IDEAS, selector)
-    vip = VICIdeasParser(content)
-    links = vip.get_ideas_links()
-    print(links)
+    url_idea = f'{URL_VIC}/idea/INMUNE_BIO_INC/3528803511'
+    content = d.get(url_idea)
+    parsed = IdeaParser(content)
+    print(parsed.get_publication_date())
+
+
+# with Driver() as d:
+#     url_vic_ideas = f'{URL_VIC}/ideas'
+#     selector = (By.CSS_SELECTOR, "#ideas_body>.row")
+#     content = d.get(url_vic_ideas, selector)
+#     vip = VICIdeasParser(content)
+#     links = vip.get_ideas_links()
+#     print(links)
