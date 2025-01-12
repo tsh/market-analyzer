5e24221b6d2079c5a6597565bcb6f04f2512b470
tsh <tsh@tsh.tsh>
2025-01-12 20:14:11 +0700

    nidum model

--- playground.py
+++ playground.py
@@ -1,51 +1,60 @@
-# import requests
-#
-url_vic = 'http://valueinvestorsclub.com'
-url_vic_ideas = f'{url_vic}/ideas'
-
 url_wsb = 'https://www.reddit.com/r/wallstreetbets/'
+from pprint import pprint
+
+from transformers import AutoModelForSequenceClassification
+
+
+# Use a pipeline as a high-level helper
+# from transformers import pipeline
+# 
+# pipe = pipeline("text-classification", model="ProsusAI/finbert")  # bad
+# print(pipe(["""
+# This is a fairly simple thesis of revenue growth and margin expansion. Most of the hard work of rolling out a new product together with difficult market conditions are behind now. The company just needs to keep doing what they’re doing in order to deliver satisfactory results. Based on moderate growth assumptions BKTI is a $33 stock by the end of 2025.
+# Catalyst
+# Further revenue growth and margin expansion
+# """]))
+
+from transformers import BertTokenizer, BertForSequenceClassification, pipeline
+import transformers
+
+
+# finbert = BertForSequenceClassification.from_pretrained('yiyanghkust/finbert-tone',num_labels=3)
+# tokenizer = BertTokenizer.from_pretrained('yiyanghkust/finbert-tone')
 #
-# resp = requests.get(url)
-# resp.raise_for_status()
-# print(resp.content)
-# print('==============================\n')
+# nlp = pipeline("text-classification", model=finbert, tokenizer=tokenizer)
+# results = nlp(['This is a fairly simple thesis of revenue growth and margin expansion. Most of the hard work of rolling out a new product together with difficult market conditions are behind now. The company just needs to keep doing what they’re doing in order to deliver satisfactory results. Based on moderate growth assumptions BKTI is a $33 stock by the end of 2025.'])
 #
-
-fname = 'file.html'
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
+# print(results)
 
 
+q_stock_name = [
+    {"role": "user", "content": "answer in valid json like this {'ticker': 'stock name'}, what is the stock ticker: BKTI is a microcap and is best suited for small funds and PAs"},
+]
 
-# Configure Chrome options
-# options = Options()
-# options.add_argument("--headless=new")
-# options.add_argument("--window-size=1920,1200")
-# 
-# service = Service()
-# service.executable_path = '/lib/chromedriver'
-# 
-# # Initialize the Chrome driver with the specified options
-# 
-# driver = webdriver.Chrome(options=options)
-# driver.get(url_vic_ideas)
-# ideas = driver.find_element(By.CSS_SELECTOR, "#ideas_body>.row")
-# 
-# WebDriverWait(driver, 5).until(lambda x: ideas.is_displayed() )
-# 
-# links = ideas.find_elements(By.TAG_NAME, 'a')
-# with open(fname, 'w') as f:
-#     f.write(driver.page_source)
-# It's a good practice to close the driver when you're finished
-# driver.quit()
+q_stock_price = [
+    {"role": "user", "content": "answer in valid json like this {'price': 42} what is the stock price: This is a fairly simple thesis of revenue growth and margin expansion. Most of the hard work of rolling out a new product together with difficult market conditions are behind now. The company just needs to keep doing what they’re doing in order to deliver satisfactory results. Based on moderate growth assumptions BKTI is a $33 stock by the end of 2025." }
+]
+# from transformers import pipeline
+# pipe = pipeline("text-generation", model="FINGU-AI/FinguAI-Chat-v1")
 
-with open(fname, 'r') as f:
-    content = f.read()
+# pprint(pipe(q_stock_price))  # bad
+# pprint(pipe(q_stock_name))   # good
 
-from bs4 import BeautifulSoup, SoupStrainer
 
-for link in  BeautifulSoup(content, 'html.parser').find(id='ideas_body').find_all('a'):
-    print(url_vic + link.get('href'))
+import torch
+from transformers import pipeline, PretrainedConfig
+
+pipe = pipeline(
+    "text-generation",
+    model="nidum/Nidum-Llama-3.2-3B-Uncensored",  # VERY GOOD, very slow. There are lighter models
+    model_kwargs={"torch_dtype": torch.bfloat16},
+    config=PretrainedConfig(name_or_path='model-Q2_K')
+)
+
+
+outputs = pipe(q_stock_price, max_new_tokens=256)
+assistant_response = outputs[0]["generated_text"][-1]["content"].strip()
+print(assistant_response)
+
+
+
