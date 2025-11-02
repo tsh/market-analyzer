18621ffc6c9f9c4427f026d9a2dd7c974e5df664
Anton Nikulin <dr.tallin@gmail.com>
2025-11-02 13:40:07 +0700

    download ticker

---
+++ instruments/correlation.py
@@ -0,0 +1,23 @@
+
+import os
+
+import yfinance as yf
+
+import config
+
+
+class DownloadTickerData:
+    @classmethod
+    def download(cls, ticker):
+        df = yf.download(
+        tickers = [ticker],
+        period = "50y",
+        interval = "1d"
+        )
+        min_date = df.index[0].strftime('%Y-%m-%d')
+        max_date = df.index[-1].strftime('%Y-%m-%d')
+        filename = f'{ticker}_{min_date}_{max_date}_OHLC.parquet'
+        path = os.path.join(config.INSTRUMENT_DATA_DIR, filename)
+        df.to_parquet(path, compression='zstd')
+        
+# df['Close'].dropna().corr()
